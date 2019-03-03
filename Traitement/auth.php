3e0ced987e1c2b2f7db15d13863f54e0193bd6f6
Kawtar <tajanikawtar@gmail.com>
2019-03-03 21:13:36 +0100

    achraf_secondCommit

---
+++ Traitement/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+$login  =  isset($_POST["inputLogin"])  ? $_POST["inputLogin"] : "";
+$MDP  =  isset($_POST["inputMDP"])  ? $_POST["inputMDP"] : "";
+
+
+$req = "select * from users where login = '$login' and mdp  = '".md5($MDP)."' ";
+echo $req;
+?>
