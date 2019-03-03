167bcc3d38db5bda23a28aae58df981feaa1b73a
Kawtar <tajanikawtar@gmail.com>
2019-03-03 21:22:19 +0100

    second

--- Traitement/auth.php
+++ Traitement/auth.php
@@ -3,7 +3,7 @@
 $login  =  isset($_POST["inputLogin"])  ? $_POST["inputLogin"] : "";
 $MDP  =  isset($_POST["inputMDP"])  ? $_POST["inputMDP"] : "";
 
-
 $req = "select * from users where login = '$login' and mdp  = '".md5($MDP)."' ";
 echo $req;
+
 ?>
